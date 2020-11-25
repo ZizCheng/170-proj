bc8571431415a915a22b5ef2f7f7c310e7e67454
Saahil Parikh <saahilparikh2000@gmail.com>
2020-11-24 16:43:00 -0800

    made input gen

--- src/pt1/parser.py
+++ src/pt1/parser.py
@@ -4,25 +4,12 @@ def getParams(filename) :
     n = 0
     stress_budget = 0
     data = []
-    with open(filename) as f:
-        read_data = int(f.readline)
-        stress_budget = float(f.readline)
+    with open("input/" + filename) as f:
+        n = f.readline
+        stress_budget = f.readline
         for i in range (0, n):
             student = f.readline
             data.append(student.split(" "))
     # We can check that the file has been automatically closed.
     f.closed
-    data = convertDataToInts(data)
     return n, stress_budget, data
-
-def convertDataToInts(data):
-    for i in data:
-        track = 0
-        for e in len(i):
-            if track <= 1:
-                i[e] = int(i[e])
-            else:
-                i[e] = float(i[e])
-            track += 1
-    return data
-    
--- src/pt1/randominputgenerator.py
+++ src/pt1/randominputgenerator.py
@@ -1,13 +1,13 @@
 import random
 
 def inputGen (num_students, filename):
-    f = open(filename, 'w')
+    f = open("input/" + filename, 'w')
     f.write(str(num_students) + '\n')
-    f.write(str(random.random()*100) + '\n')
+    f.write(str(round(random.random()*100, 3)) + '\n')
     for i in range(0, num_students):
         for j in range(0, num_students):
             if i is not j:
-                f.write(str(i) + ' ' + str(j) + ' ' + str(random.random()*100) +' ' +str(random.random()*100) + '\n')
+                f.write(str(i) + ' ' + str(j) + ' ' + str(round(random.random()*100, 3)) +' ' +str(round(random.random()*100, 3)) + '\n')
     
     
     
