d5a763208897699f5a2836818977465429e3e148
Saahil Parikh <saahilparikh2000@gmail.com>
2020-12-01 22:35:10 -0800

    works

--- src/pt2/solver.py
+++ src/pt2/solver.py
@@ -1,6 +1,6 @@
 import networkx as nx
 from parse import read_input_file, write_output_file
-from utils import is_valid_solution, calculate_happiness
+from utils import is_valid_solution, calculate_happiness, calculate_stress_for_room, convert_dictionary
 import sys
 import random
 from math import floor
@@ -15,33 +15,133 @@ def solve(G, s):
         k: Number of breakout rooms
     """
     
+    #return greedyHappinessTraverser(G, s)
     return brute_forcer(G, s, 3)
+    #return randomizedSolver(G, s, 1000)
+    #return initDictionary(len(G.nodes)), 1
 
-    
     pass
+
+def greedyHappinessTraverser(G, s):
+    # This algo uses a starting point of highest theoretical happiness: 1 room. 
+    # Every move that the algo makes is to minimize the happiness lost by each turn.
+
+    num_students = len(G.nodes)
+    singleroom_dictionary = initDictionary(num_students)
+
+    if is_valid_solution(singleroom_dictionary, G, s, 1):
+        return singleroom_dictionary, s
+    
+    max_happy = 0
+    numrooms = 2
+    current_dictionary = {0: [i for i in range(0, num_students)]}
+    found = False
+
+    while not found:
+        for k in range(2, num_students - 1):
+            current_dictionary[k-1] = []
+            sat = False
+            while not sat:
+
+                current_dictionary = makeMove(current_dictionary, k, G)
+
+                if is_valid_solution(convert_dictionary(current_dictionary), G, s, k):
+                    found = True
+                    sat = True
+                    numrooms = k
+    return current_dictionary, k
+
+def getStressList(current_dictionary, k):
+    stress_list = []
+    for _ in range(0, k):
+        stress_list.append(calculate_stress_for_room(convert_dictionary(current_dictionary).get(k), G))
+    return stress_list
+
+def makeMove(dic, k, G):
+    stress = getStressList(dic, k)
+
+    max_stressed_room = stress.index(max(stress))
+    min_stressed_room = stress.index(min(stress))
+    maxH = -1
+    maxDic = dic
+    tempdic = []
+    for i in dic.get(max_stressed_room):
+        tempdic = dic
+        print(tempdic, i, max_stressed_room, min_stressed_room)
+        tempdic[min_stressed_room] = tempdic.get(min_stressed_room).append(i)
+        tempdic[max_stressed_room] = tempdic.get(max_stressed_room).remove(i)
+        currentH = calculate_happiness(convert_dictionary(tempdic), G)
+        if currentH > maxH:
+            maxH = currentH
+            maxDic = tempdic
+    return maxDic
+
+
+def greedySolutionFinder(G, s):
+    # This algo uses a starting point of highest theoretical happiness: 1 room, and expands rooms from there trying to just find a valid solution. 
+    # The pretense is that the smaller the number of rooms the more likey the solution is more optimal than a solution with +n more rooms. 
+
+    num_students = len(G.nodes)
+    singleroom_dictionary = initDictionary(max_breakout_rooms)
+
+    if is_valid_solution(singleroom_dictionary, G, s, 1):
+        return singleroom_dictionary, s
+    
+    max_happy = 0
+    numrooms = 2
+    current_dictionary = {0: [i for i in range(0, num_students)]}
+    found = False
+
+    # Go through all the rooms and find the room with the most stress. In that room, find the individual causing the most stress. Move that individual.
+    while not found:
+        max_stress_room = -1
+        max_stress = -1
+        sat = True
+        for k in range(0, 2):
+            if calculate_stress_for_room(current_dictionary.get(k, default=0)):
+                sat = 111
+
+    # If moving that individual puts all rooms below the budget s, then a solution is found
+
+    # TODO: optimize happiness on a valid solution.
+    return 1
+
+
+
+
+
+def find_trivial(G, s):
+    best_dictionary = initDictionary(len(G.nodes))
+    if is_valid_solution(best_dictionary, G, s, 1):
+        return best_dictionary, 1
+    return False
 
 def brute_forcer(G, s, depth):
     max_breakout_rooms = len(G.nodes)
-    best_dictionary = initDictionary(max_breakout_rooms)
-    if is_valid_solution(best_dictionary, G, s, 1):
-        max_happy = calculate_happiness(best_dictionary, G)
-    else:
-        max_happy = 0
+    ans = find_trivial(G, s)
+    if ans is True:
+        return ans[0], ans[1]
+
+    max_happy = -1
     numrooms = -1
-    current_dictionary = best_dictionary
+    best = initDictionary(max_breakout_rooms)
     for k in range(2, min(depth, max_breakout_rooms+1)):
         still_going = True
         current_dictionary = initDictionary(max_breakout_rooms)
+        ct = 0
         while still_going:
+            if ct%1000 == 0:
+                print(ct, end=", ")
+            ct = ct + 1
             current_dictionary, still_going = permuteDictionary(current_dictionary, max_breakout_rooms, k)
             if is_valid_solution(current_dictionary, G, s, k):
                 currhappy = calculate_happiness(current_dictionary, G)
                 if currhappy > max_happy:
                     max_happy = currhappy
-                    best_dictionary = current_dictionary
+                    best = current_dictionary.copy()
                     numrooms = k
-        print(max_happy)
-    return best_dictionary, numrooms
+    print("HAPPINESS: ", max_happy)
+    return best, numrooms
     
 def randomizedSolver(G, s, checker_resolution):
     max_breakout_rooms = len(G.nodes)
@@ -55,7 +155,7 @@ def randomizedSolver(G, s, checker_resolution):
         for j in range(0, checker_resolution):
             if j%10000 is 0:
                 print(str(k) + ' ' + str(j))
-            current_dictionary = randomlyDictionary(max_breakout_rooms, k)
+            current_dictionary = randomlyDictionary(max_breakout_rooms, k - 1)
             if is_valid_solution(current_dictionary, G, s, k):
                 currhappy = calculate_happiness(current_dictionary, G)
                 if currhappy > max_happy:
@@ -114,8 +214,18 @@ if __name__ == '__main__':
         output_path = 'output/small/' + input_path[-6:-3] + '.out'
         G, s = read_input_file(input_path, 100)
         D, k = solve(G, s)
-        print(G)
-        print(input_path)
+        print(D)
         if is_valid_solution(D, G, s, k):
             cost_t = calculate_happiness(D, G)
             write_output_file(D, output_path)
+
+# if __name__ == '__main__':
+#     inputs = glob.glob('/input/small/*')
+#     for input_path in inputs:
+#         output_path = '/output/small/' + basename(normpath(input_path))[:-3] + '.out'
+#         G, s = read_input_file(input_path, 100)
+#         D, k = solve(G, s)
+#         print(D)
+#         assert is_valid_solution(D, G, s, k)
+#         cost_t = calculate_happiness(T)
+#         write_output_file(D, output_path)
